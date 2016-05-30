c3bf2c7c8dea665a5138c8b1efc15e1bdd85a343
HidemitsuAsano <windful.heart@gmail.com>
2016-05-30 17:59:15 +0900

    update clustering

--- simDet/Analyzer/src/TrHitCluster.hh
+++ simDet/Analyzer/src/TrHitCluster.hh
@@ -19,28 +19,28 @@ public:
 private:
   //TrLTrackHit *hitA_, *hitB_;
   int clustersize_; // number of hit fiber (MPPC)
-  int clusterlxsize_; // cluster size in local x axis
+  float clusterlxsize_; // cluster size in local x axis
   int clusterlzsize_; // 1 or 2 (max number = number of sublayer)
   unsigned int clusterID_;
-  double localx_;//mean (maybe weighted mean) of TrHits
-  double adcsum_;//sum of adc of each MPPC
+  float localx_;//mean (maybe weighted mean) of TrHits
+  float adcsum_;//sum of adc of each MPPC
                  //TODO : how to deal with TDC info ?
-  double tiltangle_; // maybe don't need to implement here, since tiltangle and z position can be obtained from the confmanager
-  double zpos_;      //
+  float tiltangle_; // maybe don't need to implement here, since tiltangle and z position can be obtained from the confmanager
+  float zpos_;      //
 
 public:
   unsigned int GetClusterID(void) const { return clusterID_; }
   void SetClusterID(unsigned int id)  { clusterID_ = id; }
   int GetClusterSize( void ) const { return clustersize_; }
   void SetClusterSize(int size) { clustersize_ = size; }
-  int GetClusterLxSize( void ) const { return clusterlxsize_; }
-  void SetClusterLxSize(int lxsize) { clusterlxsize_ = lxsize; }
+  float GetClusterLxSize( void ) const { return clusterlxsize_; }
+  void SetClusterLxSize(float lxsize) { clusterlxsize_ = lxsize; }
   int GetClusterLzSize( void ) const { return clusterlzsize_; }
   void SetClusterLzSize(int lzsize) { clusterlzsize_ = lzsize; }
-  double GetLocalX( void ) const { return localx_; }
-  void SetLocalX(double lx )  { localx_=lx; }
-  double GetAdcSum( void ) const { return adcsum_; }
-  void SetAdcSum(double adc )  { adcsum_=adc; }
+  float GetLocalX( void ) const { return localx_; }
+  void SetLocalX(float lx )  { localx_=lx; }
+  float GetAdcSum( void ) const { return adcsum_; }
+  void SetAdcSum(float adc )  { adcsum_=adc; }
   
   /*
   TrLTrackHit *GetHit( int i ) const
