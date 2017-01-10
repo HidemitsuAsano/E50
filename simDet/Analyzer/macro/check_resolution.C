53933ba1d81b322b421fe87342e1fcca62ee019c
HidemitsuAsano <windful.heart@gmail.com>
2017-01-10 23:47:10 +0900

    macro

--- simDet/Analyzer/macro/check_resolution.C
+++ simDet/Analyzer/macro/check_resolution.C
@@ -1,16 +1,17 @@
 void check_resolution(const char* filename)
 {
+  gStyle->SetOptStat("emr");
   TFile::Open(filename);
   TCanvas *cdx0 = new TCanvas("cdx0","cdx0");
-  tree->Draw("x0diff>>hdx0(100,-4,4)");
+  tree->Draw("x0diff>>hdx0(100,-1,1)");
   hdx0->SetXTitle("reconstructed x0 - generated x0 (mm)");
-  hdx0->Fit("gaus","","",-4,4);
+  hdx0->Fit("gaus","","",-0.5,0.5);
   
 
   TCanvas *cdy0 = new TCanvas("cdy0","cdy0");
-  tree->Draw("y0diff>>hdy0(100,-4,4)");
+  tree->Draw("y0diff>>hdy0(100,-1,1)");
   hdy0->SetXTitle("reconstructed y0 - generated y0 (mm)");
-  hdy0->Fit("gaus","","",-4,4);
+  hdy0->Fit("gaus","","",-0.5,0.5);
 
   TCanvas *craw = new TCanvas("craw","craw");
   craw->Divide(2,1);
@@ -18,7 +19,7 @@ void check_resolution(const char* filename)
   tree->Draw("x0>>hx0(100,-6,6)");
   tree->Draw("priposx>>hprix(100,-6,6)");
   hprix->SetLineColor(2);
-  hx0->SetXTitle("projected x position at (z=0) (mm)"); 
+  hx0->SetXTitle("x position at (z=0) (mm)"); 
   hx0->Draw();
   hprix->Draw("same");
   
@@ -26,8 +27,17 @@ void check_resolution(const char* filename)
   tree->Draw("y0>>hy0(100,-7,7)");
   tree->Draw("priposy>>hpriy(100,-7,7)");
   hpriy->SetLineColor(2);
-  hy0->SetXTitle("projected y position at (z=0) (mm)"); 
+  hy0->SetXTitle("y position at (z=0) (mm)"); 
   hy0->Draw();
   hpriy->Draw("same");
 
+  TCanvas *cres = new TCanvas("cres","cres");
+  cres->cd();
+  tree->Draw("res");
+
+  //TCanvas *c_angleres = new TCanvas("c_angleres","c_angleres")
+  //
+  
+
+  
 }
